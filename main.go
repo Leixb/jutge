f9b5ad22779e24dae794bbb98a968180739dbbaa
Leixb <abone9999@gmail.com>
2019-07-12 01:34:14 +0200

    Added default value to work-dir

--- main.go
+++ main.go
@@ -23,7 +23,7 @@ var Conf GlobalConfig
 func main() {
 	app := kingpin.New("jutge_go", "Jutge.org CLI implemented in go")
 
-	app.Flag("work-dir", "Directory to save jutge files").Envar("JUTGE_WD").StringVar(&Conf.WorkDir)
+	app.Flag("work-dir", "Directory to save jutge files").Default("JutgeProblems").Envar("JUTGE_WD").StringVar(&Conf.WorkDir)
 	app.Flag("regex", "Code regex").Default(`[PGQX]\d{5}_(ca|en|es)`).StringVar(&Conf.Regex)
 	app.Flag("verbosity", "Verbosity level").Short('v').CounterVar(&Conf.Verbosity)
 	app.Flag("quiet", "Suppress output").Short('q').BoolVar(&Conf.Quiet)
