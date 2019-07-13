b230353e390d3bcf484d0c3dbf2948a02be7a0e9
Leixb <abone9999@gmail.com>
2019-07-13 18:19:53 +0200

    Added fr and de to regex

--- main.go
+++ main.go
@@ -42,7 +42,7 @@ func main() {
 	app := kingpin.New(os.Args[0], "Jutge.org CLI implemented in go")
 
 	app.Flag("work-dir", "Directory to save jutge files").Default("JutgeProblems").Envar("JUTGE_WD").StringVar(&Conf.WorkDir)
-	app.Flag("regex", "Code regex").Default(`[PGQX]\d{5}_(ca|en|es)`).StringVar(&Conf.Regex)
+	app.Flag("regex", "Code regex").Default(`[PGQX]\d{5}_(ca|en|es|fr|de)`).StringVar(&Conf.Regex)
 	app.Flag("verbosity", "Verbosity level").Short('v').CounterVar(&Conf.Verbosity)
 	app.Flag("quiet", "Suppress output").Short('q').BoolVar(&Conf.Quiet)
 
