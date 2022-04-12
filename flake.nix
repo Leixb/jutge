35aa6d05dd9890c817114f4393734b672944250a
LeixB <abone9999@gmail.com>
2022-04-12 13:39:34 +0200

    Add flake

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+  };
+
+  outputs = { self, nixpkgs, flake-utils, }:
+
+  flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+    in
+    rec {
+      devShell = pkgs.mkShellNoCC {
+
+        buildInputs = with pkgs; [
+          go_1_18
+          gopls
+          gotools
+        ];
+
+      };
+
+      packages.jutge = pkgs.buildGo118Module {
+        pname = "jutge";
+        version = "0.3.1";
+        src = ./.;
+        vendorSha256 = "sha256-Cz5/qVzIFmBdnq0EEslhCodJgyqYPRKy9+R5IZTvGDI=";
+      };
+
+      defaultPackage = packages.jutge;
+    }
+  );
+}
